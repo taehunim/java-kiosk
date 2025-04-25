5cecf71227574b2b0c3f31cd6af18c70366366ae
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-25 17:25:57 +0900

    feat : 메뉴판 출력

--- src/com/taehunim/kiosk/levelone/Main.java
+++ src/com/taehunim/kiosk/levelone/Main.java
@@ -1,6 +1,19 @@
 package com.taehunim.kiosk.levelone;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[Real Buger 메뉴]");
+        System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+        System.out.println("0. 종료           | 종료");
+
+
+
     }
 }
