966690c9eb168d8f0aa8f2ca4b6f6a09c7efb992
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-25 17:38:46 +0900

    feat : ì사용자 ë메ë가 입력한 문자열 ëì문자열 숫자로 변í환 후 유효성 검사

--- src/com/taehunim/kiosk/levelone/Main.java
+++ src/com/taehunim/kiosk/levelone/Main.java
@@ -5,14 +5,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String userInput;
+        int selectNumber;
 
-        System.out.println("[Real Buger 메뉴]");
-        System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-        System.out.println("0. 종료           | 종료");
-
+        while (true) {
+            System.out.println("[Real Buger 메뉴]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료           | 종료");
+            System.out.println("주문하시려면 원하시는 메뉴의 번호를 입력하시면 주문이 완료됩니다.");
+            System.out.println("0을 입력하시면 프로그램을 종료합니다.");
+            System.out.print("입력창 : ");
+            userInput = scanner.nextLine();
+            try {
+                selectNumber = Integer.parseInt(userInput);
+                if (selectNumber < 0 || selectNumber > 4) {
+                    System.out.println("잘못된 주문 번호 입니다.\n 다시 입력해주세요");
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 주문 번호 입니다.\n 다시 입력해주세요");
+                continue;
+            }
+            System.out.println("ShackBurger 주문 완료");
+            System.out.println("SmokeShack 주문 완료");
+            System.out.println("Cheeseburger 주문 완료");
+            System.out.println("Hamburger 주문 완료");
+            System.out.println("잘못된 주문 번호 입니다.\n 다시 입력해주세요");
+        }
 
 
     }
