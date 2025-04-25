fb25a68afef5576f62c5069b6f63bd3a8b2575c8
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-25 18:03:55 +0900

    style :코드포맷팅 후 주석처리 완료

--- src/com/taehunim/kiosk/levelone/Main.java
+++ src/com/taehunim/kiosk/levelone/Main.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         String userInput;
         int selectNumber;
 
+        // 반복 시작지점
         while (true) {
+
+            // 메뉴 출력
             System.out.println("[Real Buger 메뉴]");
             System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
             System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
@@ -17,8 +21,12 @@ public class Main {
             System.out.println("0. 종료           | 종료");
             System.out.println("주문하시려면 원하시는 메뉴의 번호를 입력하시면 주문이 완료됩니다.");
             System.out.println("0을 입력하시면 프로그램을 종료합니다.");
+
+            // 사용자 메뉴 선택
             System.out.print("입력창 : ");
             userInput = scanner.nextLine();
+
+            // 입력된 문자열 정수로 변환
             try {
                 selectNumber = Integer.parseInt(userInput);
             } catch (NumberFormatException e) {
@@ -26,6 +34,7 @@ public class Main {
                 continue;
             }
 
+            // 변환 받은 정수 판별
             switch (selectNumber) {
                 case 0 -> System.exit(0);
                 case 1 -> System.out.println("ShackBurger 주문 완료");
