a3ce499819d33fd514c25465d03a248dcc3474c9
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 08:58:40 +0900

    add text for MenuItem class

--- src/com/taehunim/kiosk/leveltwo/MenuItem.java
+++ src/com/taehunim/kiosk/leveltwo/MenuItem.java
@@ -1,5 +1,11 @@
 package com.taehunim.kiosk.leveltwo;
 
+/**
+ * kiosk의 음식 메뉴 정보를 제공하는 Menyiteam입니다.
+ * 이름 , 정보, 가격 세가지 필드를 가지고 있습니다.
+ * 생성자를 통해 이를 외부에서 주입받습니다.
+ * getter를 통해 외부에 정보를 전달할 수 있습니다.
+ */
 public class MenuItem {
 
     // 속성
