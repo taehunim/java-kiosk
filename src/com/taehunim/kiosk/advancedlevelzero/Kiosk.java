d6c7814acede796e1acc681048c9821ce92e3725
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 22:11:21 +0900

    try catch ãmethod 적용완료

--- src/com/taehunim/kiosk/advancedlevelzero/Kiosk.java
+++ src/com/taehunim/kiosk/advancedlevelzero/Kiosk.java
@@ -27,108 +27,111 @@ public class Kiosk {
             interaction.printHowSelectMenu();
             interaction.printInputLocation();
             String input = interaction.userTextInput();
+            inputNumber = interaction.exceptionHandlingForStringtoint(input);
             if (inputNumber == -1) {
                 interaction.pirntWarningWrongValue();
                 continue;
-            }
-            switch (inputNumber) {
-                case 0 -> {
-                    interaction.pirntExitIntro();
-                    break kioskSystem;
-                }
-                case 1 -> {
-                    interaction.printStartKioskMessage();
-                    for (int i = 0; i < totalMenu.get(0).getMenuItems().size(); i++) {
-                        System.out.println((i + 1) + ".      " +
-                                totalMenu.get(0).getMenuItems().get(i).getProductName() +
-                                "     " +
-                                totalMenu.get(0).getMenuItems().get(i).getProductPrice() +
-                                "원      " +
-                                totalMenu.get(0).getMenuItems().get(i).getProductInfo()
-                        );
+            } else {
+                switch (inputNumber) {
+                    case 0 -> {
+                        interaction.pirntExitIntro();
+                        break kioskSystem;
                     }
-                    interaction.printReturnMainInfo();
-                    interaction.printInputLocation();
-                    input = interaction.userTextInput();
-                    inputNumber = interaction.exceptionHandlingForStringtoint(input);
-                    if (inputNumber == -1) {
+                    case 1 -> {
+                        interaction.printStartKioskMessage();
+                        for (int i = 0; i < totalMenu.get(0).getMenuItems().size(); i++) {
+                            System.out.println((i + 1) + ".      " +
+                                    totalMenu.get(0).getMenuItems().get(i).getProductName() +
+                                    "     " +
+                                    totalMenu.get(0).getMenuItems().get(i).getProductPrice() +
+                                    "원      " +
+                                    totalMenu.get(0).getMenuItems().get(i).getProductInfo()
+                            );
+                        }
+                        interaction.printReturnMainInfo();
+                        interaction.printInputLocation();
+                        input = interaction.userTextInput();
+                        inputNumber = interaction.exceptionHandlingForStringtoint(input);
+                        if (inputNumber == -1) {
+                            interaction.pirntWarningWrongValue();
+                            continue;
+                        }
+                        if (inputNumber == 0) {
+                            interaction.pirntWarningWrongValue();
+                            continue;
+                        } else if (inputNumber >= 1 && inputNumber <= totalMenu.get(0).getMenuItems().size()) {
+                            System.out.println(
+                                    totalMenu.get(0).getMenuItems().get(inputNumber - 1).getProductName() +
+                                            " 주문완료"
+                            );
+                        }
+                    }
+                    case 2 -> {
+                        interaction.printStartKioskMessage();
+                        for (int i = 0; i < totalMenu.get(1).getMenuItems().size(); i++) {
+                            System.out.println((i + 1) + ".      " +
+                                    totalMenu.get(1).getMenuItems().get(i).getProductName() +
+                                    "     " +
+                                    totalMenu.get(1).getMenuItems().get(i).getProductPrice() +
+                                    "원      " +
+                                    totalMenu.get(1).getMenuItems().get(i).getProductInfo()
+                            );
+                        }
+                        interaction.printReturnMainInfo();
+                        interaction.printInputLocation();
+                        input = interaction.userTextInput();
+                        inputNumber = interaction.exceptionHandlingForStringtoint(input);
+                        if (inputNumber == -1) {
+                            interaction.pirntWarningWrongValue();
+                            continue;
+                        }
+                        if (inputNumber == 0) {
+                            interaction.pirntWarningWrongValue();
+                            continue;
+                        } else if (inputNumber >= 1 && inputNumber <= totalMenu.get(1).getMenuItems().size()) {
+                            System.out.println(
+                                    totalMenu.get(1).getMenuItems().get(inputNumber - 1).getProductName() +
+                                            " 주문완료"
+                            );
+                        }
+                    }
+                    case 3 -> {
+                        interaction.printStartKioskMessage();
+                        for (int i = 0; i < totalMenu.get(2).getMenuItems().size(); i++) {
+                            System.out.println((i + 1) + ".      " +
+                                    totalMenu.get(2).getMenuItems().get(i).getProductName() +
+                                    "     " +
+                                    totalMenu.get(2).getMenuItems().get(i).getProductPrice() +
+                                    "원      " +
+                                    totalMenu.get(2).getMenuItems().get(i).getProductInfo()
+                            );
+                        }
+                        interaction.printReturnMainInfo();
+                        interaction.printInputLocation();
+                        input = interaction.userTextInput();
+                        inputNumber = interaction.exceptionHandlingForStringtoint(input);
+                        if (inputNumber == -1) {
+                            interaction.pirntWarningWrongValue();
+                            continue;
+                        }
+                        if (inputNumber == 0) {
+                            interaction.pirntWarningWrongValue();
+                            continue;
+                        } else if (inputNumber >= 1 && inputNumber <= totalMenu.get(2).getMenuItems().size()) {
+                            System.out.println(
+                                    totalMenu.get(2).getMenuItems().get(inputNumber - 1).getProductName() +
+                                            " 주문완료"
+                            );
+                        }
+                    }
+                    default -> {
                         interaction.pirntWarningWrongValue();
                         continue;
                     }
-                    if (inputNumber == 0) {
-                        interaction.pirntWarningWrongValue();
-                        continue;
-                    } else if (inputNumber >= 1 && inputNumber <= totalMenu.get(0).getMenuItems().size()) {
-                        System.out.println(
-                                totalMenu.get(0).getMenuItems().get(inputNumber - 1).getProductName() +
-                                        " 주문완료"
-                        );
-                    }
-                }
-                case 2 -> {
-                    interaction.printStartKioskMessage();
-                    for (int i = 0; i < totalMenu.get(1).getMenuItems().size(); i++) {
-                        System.out.println((i + 1) + ".      " +
-                                totalMenu.get(1).getMenuItems().get(i).getProductName() +
-                                "     " +
-                                totalMenu.get(1).getMenuItems().get(i).getProductPrice() +
-                                "원      " +
-                                totalMenu.get(1).getMenuItems().get(i).getProductInfo()
-                        );
-                    }
-                    interaction.printReturnMainInfo();
-                    interaction.printInputLocation();
-                    input = interaction.userTextInput();
-                    if (inputNumber == -1) {
-                        interaction.pirntWarningWrongValue();
-                        continue;
-                    }
-                    if (inputNumber == 0) {
-                        interaction.pirntWarningWrongValue();
-                        continue;
-                    } else if (inputNumber >= 1 && inputNumber <= totalMenu.get(1).getMenuItems().size()) {
-                        System.out.println(
-                                totalMenu.get(1).getMenuItems().get(inputNumber - 1).getProductName() +
-                                        " 주문완료"
-                        );
-                    }
-                }
-                case 3 -> {
-                    interaction.printStartKioskMessage();
-                    for (int i = 0; i < totalMenu.get(2).getMenuItems().size(); i++) {
-                        System.out.println((i + 1) + ".      " +
-                                totalMenu.get(2).getMenuItems().get(i).getProductName() +
-                                "     " +
-                                totalMenu.get(2).getMenuItems().get(i).getProductPrice() +
-                                "원      " +
-                                totalMenu.get(2).getMenuItems().get(i).getProductInfo()
-                        );
-                    }
-                    interaction.printReturnMainInfo();
-                    interaction.printInputLocation();
-                    input = interaction.userTextInput();
-                    if (inputNumber == -1) {
-                        interaction.pirntWarningWrongValue();
-                        continue;
-                    }
-                    if (inputNumber == 0) {
-                        interaction.pirntWarningWrongValue();
-                        continue;
-                    } else if (inputNumber >= 1 && inputNumber <= totalMenu.get(2).getMenuItems().size()) {
-                        System.out.println(
-                                totalMenu.get(2).getMenuItems().get(inputNumber - 1).getProductName() +
-                                        " 주문완료"
-                        );
-                    }
-                }
-                default -> {
-                    interaction.pirntWarningWrongValue();
-                    continue;
                 }
             }
         }
     }
-
 }
 
 
