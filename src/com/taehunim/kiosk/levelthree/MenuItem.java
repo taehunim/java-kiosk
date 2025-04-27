ddaa55431c6819ada8b2c44a7b65251ad6a377b3
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 12:39:17 +0900

    오타 수정

--- src/com/taehunim/kiosk/levelthree/MenuItem.java
+++ src/com/taehunim/kiosk/levelthree/MenuItem.java
@@ -1,7 +1,7 @@
 package com.taehunim.kiosk.levelthree;
 
 /**
- * kiosk의 음식 메뉴 정보를 제공하는 Menyitem class입니다.
+ * kiosk의 음식 메뉴 정보를 제공하는 Menuitem class입니다.
  * 이름 , 정보, 가격 세가지 필드를 가지고 있습니다.
  * 생성자를 통해 이를 외부에서 주입받습니다.
  * getter를 통해 외부에 정보를 전달할 수 있습니다.
