4931bbb5087c1c57d970b284b77f834295c2bb32
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 12:37:09 +0900

    Menu class 작성 완료

--- src/com/taehunim/kiosk/levelfour/Menu.java
+++ src/com/taehunim/kiosk/levelfour/Menu.java
@@ -1,4 +1,27 @@
 package com.taehunim.kiosk.levelfour;
 
+import java.util.List;
+
 public class Menu {
+
+    // 속성
+    List<MenuItem> burgerMenuList;
+    List<MenuItem> drinkMenuList;
+
+    String category;
+
+    // 생성자
+    Menu (String category,
+          List<MenuItem> burgerMenuList,
+          List<MenuItem> drinkMenuList) {
+        this.category = category;
+        this.burgerMenuList = burgerMenuList;
+        this.drinkMenuList = drinkMenuList;
+    }
+
+    // 기능
+    // category 필드 값을 반환하는 기능
+    String getCategory () {
+        return category;
+    }
 }
