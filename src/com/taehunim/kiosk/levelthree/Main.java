c6b0fecf4f02ee6bfc7094ba22ba978c652c169f
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 10:50:39 +0900

    초기 세팅

---
+++ src/com/taehunim/kiosk/levelthree/Main.java
@@ -0,0 +1,10 @@
+package com.taehunim.kiosk.levelthree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Kiosk kiosk = new Kiosk();
+    }
+}
