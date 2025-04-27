83580fdfe57b7ca8fc0b0a787daba3d3b656f296
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 23:30:14 +0900

    도전과제 재설계

--- src/com/taehunim/kiosk/advancedlevelone/MenuSelector.java
+++
@@ -1,7 +0,0 @@
-package com.taehunim.kiosk.advancedlevelone;
-
-import java.util.List;
-
-public interface MenuSelector {
-    List<MenuItem> getMenuItems ();
-}
--- src/com/taehunim/kiosk/advancedlevelzero/MenuSelector.java
+++
@@ -1,7 +0,0 @@
-package com.taehunim.kiosk.advancedlevelzero;
-
-import java.util.List;
-
-public interface MenuSelector {
-    List<MenuItem> getMenuItems ();
-}
