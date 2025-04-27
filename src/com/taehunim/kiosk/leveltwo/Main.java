d8da0aece2dea0e6f6291a94728bacf7bc4f479b
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 09:47:10 +0900

    change name ã'menuItem -> menuItems 제시된 요구사항 이행

--- src/com/taehunim/kiosk/leveltwo/Main.java
+++ src/com/taehunim/kiosk/leveltwo/Main.java
@@ -15,25 +15,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<MenuItem> menuItem = new ArrayList<>();
+        List<MenuItem> menuItems = new ArrayList<>();
 
         String userInput;
         int selectNumber;
 
         // collection을 활용해 Menuitem 저장
-        menuItem.add(new MenuItem(
+        menuItems.add(new MenuItem(
                 "ShackBuger",
                 6900,
                 "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem(
+        menuItems.add(new MenuItem(
                 "SmokeShack",
                 8900,
                 "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem(
+        menuItems.add(new MenuItem(
                 "Cheeseburger",
                 6900,
                 "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem(
+        menuItems.add(new MenuItem(
                 "Hamburger",
                 5400,
                 "비프패티를 기반으로 야채가 들어간 기본버거"));
@@ -43,8 +43,8 @@ public class Main {
         while (true) {
 
             // 메뉴 출력
-            for (int i = 0; i < menuItem.size(); i++) {
-                MenuItem menu = menuItem.get(i);
+            for (int i = 0; i < menuItems.size(); i++) {
+                MenuItem menu = menuItems.get(i);
                 System.out.println((i + 1) + ". " + menu.getBugerName() + "   | W" +
                         (menu.getBugerPrice() * 0.001) + " | " + menu.getBugerInfo());
             }
@@ -62,14 +62,14 @@ public class Main {
             }
 
             // 변환 받은 정수 판별
-            if (selectNumber > menuItem.size() || selectNumber < 0) {
+            if (selectNumber > menuItems.size() || selectNumber < 0) {
                 System.out.println("잘못된 주문 번호 입니다.\n 다시 입력해주세요");
                 continue;
             } else if (selectNumber == 0) {
                 System.out.println("프로그램을 종료합니다.");
                 break kioskSystem;
             } else {
-                System.out.println(menuItem.get(selectNumber - 1).getBugerName() + " 주문 완료");
+                System.out.println(menuItems.get(selectNumber - 1).getBugerName() + " 주문 완료");
             }
         }
     }
