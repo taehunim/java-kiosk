494ee5a70beef47601b8b009ffc74fd79b2bec61
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 09:41:42 +0900

    이쁘ã게 정렬완료

--- src/com/taehunim/kiosk/leveltwo/Main.java
+++ src/com/taehunim/kiosk/leveltwo/Main.java
@@ -13,13 +13,21 @@ public class Main {
         int selectNumber;
 
         // collection을 활용해 Menuitem 저장
-        menuItem.add(new MenuItem("ShackBuger", 6900,
+        menuItem.add(new MenuItem(
+                "ShackBuger",
+                6900,
                 "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem("SmokeShack", 8900,
+        menuItem.add(new MenuItem(
+                "SmokeShack",
+                8900,
                 "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem("Cheeseburger", 6900,
+        menuItem.add(new MenuItem(
+                "Cheeseburger",
+                6900,
                 "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem("Hamburger", 5400,
+        menuItem.add(new MenuItem(
+                "Hamburger",
+                5400,
                 "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         // 반복문 시작
