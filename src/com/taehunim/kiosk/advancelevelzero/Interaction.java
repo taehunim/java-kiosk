dc299d5389bd6fe9a6bf4f2aa2a725d5e6de9eb6
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 18:08:21 +0900

    feat Interaction class userTextInput

--- src/com/taehunim/kiosk/advancelevelzero/Interaction.java
+++ src/com/taehunim/kiosk/advancelevelzero/Interaction.java
@@ -6,13 +6,22 @@ public class Interaction {
 
     // 속성
     private final Scanner scanner;
+
     // 생성자
     public Interaction(Scanner scanner) {
         this.scanner = scanner;
     }
 
     // 기능
+
+    public String userTextInput() {
+        return scanner.nextLine();
+    }
+
     public void printStartKioskMessage() {
         System.out.println("메뉴를 선택해주세요.");
     }
+
+
+}
 }
