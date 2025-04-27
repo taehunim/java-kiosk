f61cf818b3945fb95617a124c6646c9ac66c00f9
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 17:58:22 +0900

    interaction ëªclass 기본 설정 완료

--- src/com/taehunim/kiosk/advancelevelzero/Interaction.java
+++ src/com/taehunim/kiosk/advancelevelzero/Interaction.java
@@ -1,4 +1,18 @@
 package com.taehunim.kiosk.advancelevelzero;
 
+import java.util.Scanner;
+
 public class Interaction {
+
+    // 속성
+    private final Scanner scanner;
+    // 생성자
+    public Interaction(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    // 기능
+    public void printStartKioskMessage() {
+        System.out.println("메뉴를 선택해주세요.");
+    }
 }
