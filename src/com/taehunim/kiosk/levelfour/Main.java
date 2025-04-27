5b205e1968c2f6ef3b8ed674ab6910bcc5ea7572
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 13:41:01 +0900

    ë메인에서 Menu 객체 생성 완료

--- src/com/taehunim/kiosk/levelfour/Main.java
+++ src/com/taehunim/kiosk/levelfour/Main.java
@@ -4,52 +4,57 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 필수 객체 생성 및 Collection 생성
         Scanner scanner = new Scanner(System.in);
-        Kiosk kiosk = new Kiosk(scanner);
 
         List<MenuItem> burgerMenuItems = new ArrayList<>();
         burgerMenuItems.add(new MenuItem(
-                "ShackBurger",
-                6900,
-                "토마토, 양상추, 쉑소스가 토핑된 치즈버거"
+                        "ShackBurger",
+                        6900,
+                        "토마토, 양상추, 쉑소스가 토핑된 치즈버거"
                 )
         );
         burgerMenuItems.add(new MenuItem(
-                "SmokeShack",
-                8900,
-                "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"
+                        "SmokeShack",
+                        8900,
+                        "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"
                 )
         );
         burgerMenuItems.add(new MenuItem(
-                "Cheeseburger",
-                6900,
-                "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"
+                        "Cheeseburger",
+                        6900,
+                        "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"
                 )
         );
         burgerMenuItems.add(new MenuItem(
-                "Hamburger",
-                5400,
-                "비프패티를 기반으로 야채가 들어간 기본버거"
+                        "Hamburger",
+                        5400,
+                        "비프패티를 기반으로 야채가 들어간 기본버거"
                 )
         );
 
         List<MenuItem> drinkMenuItems = new ArrayList<>();
         drinkMenuItems.add(new MenuItem(
-                "상품이 준비중입니다.",
-                0,
-                "상품이 준비중입니다."
+                        "상품이 준비중입니다.",
+                        0,
+                        "상품이 준비중입니다."
                 )
         );
         List<MenuItem> desertMenuItems = new ArrayList<>();
         desertMenuItems.add(new MenuItem(
-                "상품이 준비중입니다.",
-                0,
-                "상품이 준비중입니다."
+                        "상품이 준비중입니다.",
+                        0,
+                        "상품이 준비중입니다."
                 )
         );
 
         Menu burgerMenu = new Menu("burger", burgerMenuItems);
         Menu drinkMenu = new Menu("drink", burgerMenuItems);
         Menu desertMenu = new Menu("desert", burgerMenuItems);
+
+        Kiosk kiosk = new Kiosk(scanner, burgerMenu, desertMenu, desertMenu);
+
     }
+
 }
