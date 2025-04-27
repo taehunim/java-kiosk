005d1d2160169227f82e3da398ee7334a06abfc4
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 12:48:38 +0900

    Menu clas 수정

--- src/com/taehunim/kiosk/levelfour/Main.java
+++ src/com/taehunim/kiosk/levelfour/Main.java
@@ -1,9 +1,14 @@
 package com.taehunim.kiosk.levelfour;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<MenuItem> burgerMenuItems = new ArrayList<>();
+        burgerMenuItems.add()
     }
 }
--- src/com/taehunim/kiosk/levelfour/Menu.java
+++ src/com/taehunim/kiosk/levelfour/Menu.java
@@ -4,24 +4,25 @@ import java.util.List;
 
 /**
  * Menu는 MenuItem 객체들을 관리하는 class입니다.
+ * Main에서 생성된 MenuItem 객체 collection을 생성자를 통해 주입받아 활용합니다.
+ * 필드로 category를 가지고 있습니다.
+ * 생성자를 통해 주입받아 활용합니다.
+ * 각 category 별로 객체를 만들어 활용합니다.
+ * category값을 반환하는 method를 가지고 있습니다.
  *
 */
 
 public class Menu {
 
     // 속성
-    List<MenuItem> burgerMenuList;
-    List<MenuItem> drinkMenuList;
+    private final List<MenuItem> menuItems;
 
-    String category;
+    private final String category;
 
     // 생성자
-    Menu (String category,
-          List<MenuItem> burgerMenuList,
-          List<MenuItem> drinkMenuList) {
+    Menu (String category, List<MenuItem> menuItems) {
         this.category = category;
-        this.burgerMenuList = burgerMenuList;
-        this.drinkMenuList = drinkMenuList;
+        this.menuItems= menuItems;
     }
 
     // 기능
