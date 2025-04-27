7ee836e39a45720b981220f63493730e371c4315
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-27 11:10:02 +0900

    Kiosk class 에 menuitems 할당

--- src/com/taehunim/kiosk/levelthree/Kiosk.java
+++ src/com/taehunim/kiosk/levelthree/Kiosk.java
@@ -1,4 +1,20 @@
 package com.taehunim.kiosk.levelthree;
 
+import java.util.List;
+
 public class Kiosk {
+
+    // 속성
+    private List<MenuItem> menuItems;
+
+    // 생성자
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    };
+
+    // 기능
+    // 시스템 시작
+    public void start() {
+
+    }
 }
--- src/com/taehunim/kiosk/levelthree/Main.java
+++ src/com/taehunim/kiosk/levelthree/Main.java
@@ -1,10 +1,39 @@
 package com.taehunim.kiosk.levelthree;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 필수 객체 생성
         Scanner scanner = new Scanner(System.in);
-        Kiosk kiosk = new Kiosk();
+
+        List<MenuItem> menuItems =new ArrayList<>();
+        menuItems.add(new MenuItem(
+                "ShackBuger",
+                6900,
+                "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(
+                "SmokeShack",
+                8900,
+                "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(
+                "Cheeseburger",
+                6900,
+                "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(
+                "Hamburger",
+                5400,
+                "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        Kiosk kiosk = new Kiosk(menuItems);
+
+        // 시작
+
+
+
+
     }
 }
