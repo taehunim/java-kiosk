2b730799fce326f4dd002e4bfadfa5b5de1c0407
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-28 16:56:44 +0900

    ãMenuItem 클래스 초안 작성 완료

--- src/com/taehunim/kiosk/advancedlevelzero/menu/MenuSelector.java
+++ src/com/taehunim/kiosk/advancedlevelzero/menu/MenuSelector.java
@@ -1,4 +1,11 @@
 package com.taehunim.kiosk.advancedlevelzero.menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuSelector {
+
+    // 속성
+    List<MenuItem> menuItems = new ArrayList<>();
+    menuItems
 }
