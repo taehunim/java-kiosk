178565c9d5faaa520bedf1ac1477e01a81f405d9
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-30 16:37:44 +0900

    레벨 4 5 완료

--- src/leveld/Kiosk.java
+++ src/leveld/Kiosk.java
@@ -31,38 +31,36 @@ public class Kiosk {
             outputCategory(categories);
 
             // 문자열 입력
-            String userInput = inputString();
+            String userInputCategory = inputString();
 
             // 문자열 정수 변환
-            int userSelectNumber = changeStringtoInt(userInput);
+            int userSelectCategoryNumber = changeStringtoInt(userInputCategory);
 
             // 조건문 사용
             // 메뉴가 제시된 후 숫자의 입력에 따라 다른 로직을 실행
             // 반복문을 사용하여 특정 번호가 입력되면 프로그램 종료
-            switch (userSelectNumber) {
+            switch (userSelectCategoryNumber) {
                 case 0 -> {
                     break kioskSystem;
                 }
-                case 1, 2, 3, 4 -> {
-                    List<MenuItem> menuItems = menus.get(userSelectNumber - 1).getMenuItems();
-                    System.out.println("[\t" + categories.get(userSelectNumber - 1) + "\t]");
-
-                    for (int i = 0; i < menuItems.size(); i++) {
-                        MenuItem menuItem = menuItems.get(i);
-                        System.out.println((i + 1) + ". " + menuItem.getProductName() + "\t| W " + (menuItem.getProductPrice() / 1000.0) + " |\t + " + menuItem.getProductInfo());
-                    }
+                case 1, 2, 3 -> {
+                    outputMenu(userSelectCategoryNumber);
+                    System.out.println();
+                    String userInputMenu = inputString();
+                    int userSelectMenuNumber = changeStringtoInt(userInputMenu);
+                    selectMenu(userSelectCategoryNumber, userSelectMenuNumber);
                 }
                 default -> System.out.println("올바른 값을 입력해주세요.");
             }
         }
     }
 
+
     // 문자열 정수 변환 메소드
     public int changeStringtoInt(String userInput) {
         try {
             return Integer.parseInt(userInput);
         } catch (NumberFormatException e) {
-            System.out.println("올바른 값을 입력해주세요.");
             return -1;
         }
     }
@@ -80,5 +78,34 @@ public class Kiosk {
             System.out.println((i + 1) + ". " + categories.get(i));
         }
         System.out.println("0. 종료\t\t\t|  종료  |");
+    }
+
+    public void outputMenu(int userSelectNumber) {
+        List<MenuItem> menuItems = menus.get(userSelectNumber - 1).getMenuItems();
+        System.out.println("[\t" + categories.get(userSelectNumber - 1) + "\t]");
+
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem menuItem = menuItems.get(i);
+            System.out.println((i + 1) + ". " + menuItem.getProductName() + "\t| W " + (menuItem.getProductPrice() / 1000.0) + " |\t" + menuItem.getProductInfo());
+
+        }
+        System.out.println("0. 뒤로가기\t\t\t|  뒤로가기  |");
+    }
+
+    public void selectMenu(int userSelectCategoryNumber, int userSelectMenuNumber) {
+        List<MenuItem> menuItems = menus.get(userSelectCategoryNumber - 1).getMenuItems();
+        if (userSelectMenuNumber == 0) {
+        } else if (userSelectMenuNumber <= menuItems.size() && userSelectMenuNumber > 0) {
+            MenuItem menuItem = menuItems.get(userSelectMenuNumber - 1);
+            System.out.println("선택한 메뉴: " + menuItem.getProductName() + "\t| W " +
+                    (menuItem.getProductPrice() / 1000.0) + " |\t + " + menuItem.getProductInfo()
+            );
+        } else {
+            System.out.println("올바른 값을 입력해주세요");
+        }
+    }
+
+    public void addmenuItems(String category, List<MenuItem> menuItems) {
+        menus.add(new Menu(category, menuItems));
     }
 }
--- src/leveld/Menu.java
+++ src/leveld/Menu.java
@@ -19,4 +19,6 @@ public class Menu {
     public List<MenuItem> getMenuItems() {
         return menuItems;
     }
-}
+
+
+
